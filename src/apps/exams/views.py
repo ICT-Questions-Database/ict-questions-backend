06fcc9d00f8e3a5b42a37a8f131e0dee643bc634
Davi de Oliveira <olidavi031130@gmail.com>
2025-09-06 15:46:38 -0300

    feat: swagger doc for exams app

--- src/apps/exams/views.py
+++ src/apps/exams/views.py
@@ -4,7 +4,27 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import ExamAttempt, ExamQuestion
 from .serializers import ExamAttemptSerializer, ExamQuestionSerializer
+from drf_spectacular.utils import extend_schema, extend_schema_view
 
+
+@extend_schema_view(
+    list=extend_schema(
+        summary="Lists all authenticated user questions",
+        description="Lists all authenticated user questions at once.",
+    ),
+    retrieve=extend_schema(
+        summary="Retrieve a specific question",
+        description="Retrieve a specific question from the authenticated user.",
+    ),
+    start_exam=extend_schema(
+        summary="Creates an exam instance",
+        description="Creates an exam instance to be completed.",
+    ),
+    finish_exam=extend_schema(
+        summary="Updates an exam instance",
+        description="Updates an exam instance filling the incomplete data.",
+    ),
+)
 class ExamAttemptViewSet(ModelViewSet):
     serializer_class = ExamAttemptSerializer
     permission_classes = [IsAuthenticated]
@@ -48,6 +68,20 @@ class ExamAttemptViewSet(ModelViewSet):
         return Response(serializer.data)
 
 
+@extend_schema_view(
+    list=extend_schema(
+        summary="Lists all authenticated user questions",
+        description="Lists all authenticated user questions at once.",
+    ),
+    retrieve=extend_schema(
+        summary="Retrieve a specific question",
+        description="Retrieve a specific question from the authenticated user.",
+    ),
+    create=extend_schema(
+        summary="Creates an exam_question object",
+        description="Creates an exam_question object."
+    )
+)
 class ExamQuestionViewSet(ModelViewSet):
     queryset = ExamQuestion.objects.all()
     serializer_class = ExamQuestionSerializer
