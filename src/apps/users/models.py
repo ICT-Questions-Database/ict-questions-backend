37db40049e2714bd7ed6e4a073e9bc2bd392b876
Davi de Oliveira <olidavi031130@gmail.com>
2025-09-10 13:39:33 -0300

    feat: set first_name and last_name as required fields for users

--- src/apps/users/models.py
+++ src/apps/users/models.py
@@ -1,17 +1,23 @@
-from django.db.models import DateTimeField, EmailField
+from django.db.models import DateTimeField, EmailField, CharField
 from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+    first_name = CharField(max_length=150, blank=False, null=False)
+    last_name = CharField(max_length=150, blank=False, null=False)
+
+    email = EmailField(unique=True)
+
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True)
 
     USERNAME_FIELD = "email"  # Muda o campo de login padrão para email
     REQUIRED_FIELDS = [
-        "username"
-    ]  # username passa a ser obrigatório, mas email é usado pra login
+        "username",
+        "first_name",
+        "last_name",
+    ]
 
-    email = EmailField(unique=True)
 
     class Meta:
         db_table = "users"
