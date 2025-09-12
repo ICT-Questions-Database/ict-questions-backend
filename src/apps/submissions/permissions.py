c5b3c629b09268ef0ad0539629afbdced917eb5c
Davi de Oliveira <olidavi031130@gmail.com>
2025-09-12 08:25:17 -0300

    feat: customized permissions for correct_submission_answers_sources and question_submissions

--- src/apps/submissions/permissions.py
+++ src/apps/submissions/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsAdminOrOwner(BasePermission):
+class QuestionSubmissionPermission(BasePermission):
     """
     Permite admins fazer tudo.
     Usuários só podem ler e alterar/excluir submissões próprias.
@@ -14,4 +14,14 @@ class IsAdminOrOwner(BasePermission):
             return obj.submitted_by == request.user
         
         return obj.submitted_by == request.user
-    
+
+
+class CorrectSubmissionAnswersSourcesPermission(BasePermission):
+    """
+    Permite admins ou o dono da QuestionSubmission
+    acessar/editar fontes de resposta.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj.question_submission.submitted_by == request.user
