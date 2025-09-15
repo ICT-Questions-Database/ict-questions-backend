944d8a0efa89e3ddd0e9cda0fad167fbdd937867
RenannLimaa <renanroddev@gmail.com>
2025-09-15 14:02:30 -0300

    feat: add tests for QuestionViewSet

---
+++ src/apps/questions/tests/test_questions.py
@@ -0,0 +1,54 @@
+from django.contrib.auth import get_user_model
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from apps.questions.models import Question
+
+User = get_user_model()
+
+class QuestionViewSetTests(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username="student", email="student@example.com", password="pass123")
+        self.staff = User.objects.create_user(username="admin", email="admin@example.com", password="pass123", is_staff=True)
+        self.url = "http://localhost:8000/api/v1/questions/"
+
+        self.payload = {
+            "text": "New Q",
+            "level": "HCIA",
+            "has_answer": False,
+            "has_multiple_answers": False,
+            "track": "Network",
+            "weight": 1.0
+        }
+
+        self.question = Question.objects.create(
+            text="What is 2+2?",
+            submitted_by=self.user,
+            reviewed_by=self.staff,
+            level=Question.Level.HCIA,
+            track=Question.Track.COMPUTING,
+            has_answer=False,
+            has_multiple_answers=False,
+            weight=1.0
+        )
+
+    def test_list_questions_requires_auth(self):
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_list_questions_as_user(self):
+        self.client.force_authenticate(user=self.user)
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertGreaterEqual(len(res.json()["results"]), 1)
+
+    def test_create_question_forbidden_for_non_staff(self):
+        self.client.force_authenticate(user=self.user)
+        res = self.client.post(self.url, self.payload)
+        self.assertEqual(res.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_question_allowed_for_staff(self):
+        self.client.force_authenticate(user=self.staff)
+        res = self.client.post(self.url, self.payload)
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(res.data["reviewed_by"], self.staff.id)
