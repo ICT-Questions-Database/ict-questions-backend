cfb2dff3bbabd1e3d225764a4f3ce4bef0ad5239
RenannLimaa <renanroddev@gmail.com>
2025-09-17 10:05:29 -0300

    refactor: remove unnecessary GET method from correct_answers_sources

--- src/apps/questions/urls.py
+++ src/apps/questions/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path("alternatives/", AlternativeViewSet.as_view({"post": "create"})),
     path("alternatives/<int:pk>/", AlternativeViewSet.as_view({"delete": "destroy", "put": "update", "patch": "partial_update"})),
 
-    path("correct_answers_sources/", CorrectAnswersSourcesViewSet.as_view({"get": "list", "post": "create"})),
-    path("correct_answers_sources/<int:pk>/", CorrectAnswersSourcesViewSet.as_view({"get": "retrieve", "delete": "destroy", "put": "update", "patch": "partial_update"}))
+    path("correct_answers_sources/", CorrectAnswersSourcesViewSet.as_view({"post": "create"})),
+    path("correct_answers_sources/<int:pk>/", CorrectAnswersSourcesViewSet.as_view({"delete": "destroy", "put": "update", "patch": "partial_update"})),
 ]
