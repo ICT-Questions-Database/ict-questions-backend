d4c263bf9b27cd318c6014eed3439a27c84f6f76
Davi de Oliveira <olidavi031130@gmail.com>
2025-09-25 13:01:35 -0300

    chore: add url names for exams app test purposes (reverse)

--- src/apps/exams/urls.py
+++ src/apps/exams/urls.py
@@ -3,12 +3,12 @@ from .views import ExamAttemptViewSet, ExamQuestionViewSet
 
 urlpatterns = [
     # ExamAttempt
-    path("exam_attempts/", ExamAttemptViewSet.as_view({"get": "list"})),
-    path("exam_attempts/<int:pk>/", ExamAttemptViewSet.as_view({"get": "retrieve"})),
-    path("exam_attempts/start_exam/", ExamAttemptViewSet.as_view({"post": "start_exam"})),
-    path("exam_attempts/<int:pk>/finish_exam/", ExamAttemptViewSet.as_view({"patch": "finish_exam"})),
+    path("exam_attempts/", ExamAttemptViewSet.as_view({"get": "list"}), name="examattempt-list"),
+    path("exam_attempts/<int:pk>/", ExamAttemptViewSet.as_view({"get": "retrieve"}), name="examattempt-detail"),
+    path("exam_attempts/start_exam/", ExamAttemptViewSet.as_view({"post": "start_exam"}), name="examattempt-start-exam"),
+    path("exam_attempts/<int:pk>/finish_exam/", ExamAttemptViewSet.as_view({"patch": "finish_exam"}), name="examattempt-finish-exam"),
 
     # ExamQuestion
-    path("exam_questions/", ExamQuestionViewSet.as_view({"get": "list", "post": "create"})),
-    path("exam_questions/<int:pk>/", ExamQuestionViewSet.as_view({"get": "retrieve"})),
+    path("exam_questions/", ExamQuestionViewSet.as_view({"get": "list", "post": "create"}), name="examquestion-list"),
+    path("exam_questions/<int:pk>/", ExamQuestionViewSet.as_view({"get": "retrieve"}), name="examquestion-detail"),
 ]
