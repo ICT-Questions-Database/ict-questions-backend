c00a0e26cfa717e9a563c73ee657c2f8cedd84d1
Davi Oliveira <olidavi031130@gmail.com>
2026-01-26 16:11:32 -0300

    feat: add filter for questions with answer

--- src/apps/questions/filters.py
+++ src/apps/questions/filters.py
@@ -4,8 +4,9 @@ from .models import Question
 class QuestionFilter(django_filters.FilterSet):
     track = django_filters.MultipleChoiceFilter(choices=Question.Track.choices)
     level = django_filters.MultipleChoiceFilter(choices=Question.Level.choices)
+    has_answer = django_filters.BooleanFilter(field_name="has_answer")
     text = django_filters.CharFilter(field_name="text", lookup_expr="icontains")
 
     class Meta:
         model = Question
-        fields = ["track", "level", "text"]
+        fields = ["track", "level", "has_answer", "text"]
